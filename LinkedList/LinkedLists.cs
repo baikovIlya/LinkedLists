ca122f7cf096501dc5d2f77870fbfe641110a584
baikovIlya <baikusha23@gmail.com>
2022-03-25 14:44:32 +0300

    Just copy from lession

--- LinkedList/LinkedLists.cs
+++ LinkedList/LinkedLists.cs
@@ -6,7 +6,86 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    internal class LinkedLists
-    {
-    }
+        public class LinkedList
+        {
+            private Node _root;
+
+            private Node _tail;
+
+            public int this[int index]
+            {
+                get
+                {
+                    Node crnt = _root;
+                    for (int i = 1; i <= index; i++)
+                    {
+                        crnt = crnt.Next;
+                    }
+
+                    return crnt.Value;
+                }
+
+                set
+                {
+
+                }
+            }
+
+            public int Length
+            {
+                get
+                {
+                    int count = 0;
+                    Node crnt = _root;
+
+                    while (crnt != null)
+                    {
+                        crnt = crnt.Next;
+                        count++;
+                    }
+
+                    return count;
+                }
+                private set
+                {
+                }
+            }
+
+            public LinkedList()
+            {
+                _root = null;
+
+                _tail = null;
+            }
+
+            public LinkedList(int value)
+            {
+                _root = new Node(value);
+                _tail = _root;
+            }
+
+            public void Add(int value)
+            {
+                if (_root == null)
+                {
+                    _root = new Node(value);
+                    _tail = _root;
+                }
+                else
+                {
+                    //Node crnt = _root;
+
+                    //while (crnt.Next != null)
+                    //{
+                    //    crnt = crnt.Next;
+                    //}
+
+                    //crnt.Next = new Node(value);
+
+                    _tail.Next = new Node(value);
+                    _tail = _tail.Next;
+                }
+            }
+        }
+
 }
--- LinkedList/Node.cs
+++ LinkedList/Node.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace LinkedList
 {
-    internal class Node
-    {
-    }
+        public class Node
+        {
+            public int Value { get; set; }
+
+            public Node Next { get; set; }
+
+            public Node(int value)
+            {
+                Value = value;
+                Next = null;
+            }
+        }
+
 }
